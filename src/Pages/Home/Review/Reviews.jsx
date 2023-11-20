108c573f731a2561aa1dc2d6776ae778840e10f1
AvishekChy77 <u1503074@student.cuet.ac.bd>
2023-11-20 17:07:14 +0600

    added swiper in reviews

--- src/Pages/Home/Review/Reviews.jsx
+++ src/Pages/Home/Review/Reviews.jsx
@@ -1,10 +1,32 @@
+// Import Swiper React components
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Autoplay, Pagination } from "swiper/modules";
+import BookRating from "../../CategoryCollection/BookRating";
+import NavigationButtons from "./NavigationButtons";
+
 const Reviews = () => {
+  const { data: reviews } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: async () => {
+      const res = await axios.get(
+        "https://book-beacon-server.vercel.app/reviews"
+      );
+      return res.data;
+    },
+  });
+  console.log(reviews);
   return (
-    <div className="my-10">
-      <h2 className=" font-YSerif mb-5 text-2xl md:text-4xl font-bold text-blue-800">
-        Reviews
-      </h2>
-      <div className="flex gap-5 flex-col md:flex-row">
+    <div className="my-10 max-w-3xl mx-auto">
+      {/* <div className="flex gap-5 flex-col md:flex-row">
         <div className=" rounded shadow-lg bg-slate-50 p-3 flex flex-col">
           <div className="rating rating-sm">
             <input
@@ -116,7 +138,38 @@ const Reviews = () => {
           </p>
           <p className="text-blue-800">M. Jordan</p>
         </div>
-      </div>
+      </div> */}
+      <Swiper
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 1800,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper shadow-xl"
+      >
+        {reviews?.map((rev) => (
+          <SwiperSlide key={rev._id}>
+            <div className="p-8 mb-10  ">
+              <p className="text-[#D4AF37] md:pl-2">
+                <BookRating rating={rev.rating}></BookRating>
+              </p>
+              <h2 className="flex gap-2 justify-center items-center text-lg md:text-xl lg:text-2xl font-bold ">
+                <FaQuoteLeft />
+                {rev.shortQuote}
+                <FaQuoteRight />
+              </h2>
+              <p className=" text-justify text-sm md:text-base">{rev.quote}</p>
+              <p className=" font-semibold text-right">-{rev.name}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+        <NavigationButtons></NavigationButtons>
+      </Swiper>
     </div>
   );
 };
